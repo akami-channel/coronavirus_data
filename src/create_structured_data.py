42e019d3117c67914cf548518ffbf2fa4463eaf7
Your Name <you@example.com>
2020-03-03 22:35:19 +0900

    data is converted into valid JSON and the JSON is verified to be valid JSON

--- src/create_structured_data.py
+++ src/create_structured_data.py
@@ -14,14 +14,18 @@ outputColNum = 0
 counter = 0
 rowNum = 0
 
+fileOut.write("dataString = \"{")
+
 with open('hopkins_data.csv', encoding="utf-8-sig") as csvfile:
     reader = csv.DictReader(csvfile)
     # reader = csv.reader(csvfile, quoting=csv.QUOTE_NONNUMERIC) # change contents to floats
     for row in reader: # each row is a list
-    	results += str(row)
-    	# if (rowNum == 1):
-    	# 	print(row)
-    	rowNum += 1
+        results += "\'datarow" + str(rowNum) + "\'" + ": "
+        results += str(row)
+        results += ", "
+        # if (rowNum == 1):
+        # 	print(row)
+        rowNum += 1
         # for colNum, val in enumerate(row):
         #     # YELLOW DIMENSIONS ARE FROM ROW=3 to ROW=12 ....... and COL=3 to COL=6
         #     if(rowNum == 2):
@@ -34,6 +38,6 @@ with open('hopkins_data.csv', encoding="utf-8-sig") as csvfile:
         #     counter += 1
         # rowNum += 1
 
-
 fileOut.write(results)
+fileOut.write("}\";")
 fileOut.write("\n")
--- src/public/main.js
+++ src/public/main.js
@@ -5,6 +5,12 @@
 
 window.addEventListener('load', function() {
     console.log('All assets are loaded')
+    // replace single quotes with double quotes for valid JSON
+    dataString = dataString.replace(/'/g, '"');
+    // remove extra ", " at the end
+    dataString = dataString.slice(0,dataString.length - 3)
+    dataString = dataString + "}"
+    console.log(IsJsonString(dataString))
 })
 
 
