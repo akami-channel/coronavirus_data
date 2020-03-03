077da14625d9b7b911463f93e4f50ec26956e482
Your Name <you@example.com>
2020-03-03 19:39:50 +0900

    python script gets the john hopkins data

---
+++ src/get_data.py
@@ -0,0 +1,23 @@
+import pycurl
+from io import BytesIO 
+
+b_obj = BytesIO() 
+crl = pycurl.Curl() 
+
+# Set URL value
+crl.setopt(crl.URL, 'https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/03-02-2020.csv')
+
+# Write bytes that are utf-8 encoded
+crl.setopt(crl.WRITEDATA, b_obj)
+
+# Perform a file transfer 
+crl.perform() 
+
+# End curl session
+crl.close()
+
+# Get the content stored in the BytesIO object (in byte characters) 
+get_body = b_obj.getvalue()
+
+# Decode the bytes stored in get_body to HTML and print the result 
+print('Output of GET request:\n%s' % get_body.decode('utf8')) 
