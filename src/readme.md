86e8b63bb21f5bbf1d0f4018a682b2ffa921315f
Your Name <you@example.com>
2020-03-08 22:42:57 +0900

    update_data_regularly.py and r un_py_server.py should be run simultaneously to both run the server and update the data. In the html I added a meta tag that will make it reload every 5 seconds.x

--- src/readme.md
+++ src/readme.md
@@ -1,1 +1,1 @@
-server.py no longer used because I switched to php, but I left it there
+Archive contains older versions of code not being used anymore. Mainly because I switched from downloading the hopkins data from a github page to scraping the worldometers site for data
---
+++ src/update_data_regularly.py
@@ -0,0 +1,28 @@
+#!/bin/python
+
+import subprocess
+import sched, time
+
+def run_scripts():
+    bashCommand = "../download_data.sh"
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    #
+    bashCommand = "../parse_data.py"
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+    #
+    bashCommand = "../html_constructor.sh"
+    process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+    output, error = process.communicate()
+
+
+s = sched.scheduler(time.time, time.sleep)
+def do_something(sc): 
+    print("Doing stuff...")
+    # do your stuff
+    run_scripts()
+    s.enter(60, 1, do_something, (sc,))
+
+s.enter(60, 1, do_something, (s,))
+s.run()
