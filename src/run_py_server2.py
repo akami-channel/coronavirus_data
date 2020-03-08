3ace8efe54a6d7a09fa9fce453af0fc93fea0b94
Your Name <you@example.com>
2020-03-08 21:44:14 +0900

    My run_py_server2 will use bash to touch a "hi" file in the dir above public

--- src/run_py_server2.py
+++ src/run_py_server2.py
@@ -8,6 +8,13 @@ PORT = 8080
 web_dir = os.path.join(os.path.dirname(__file__), 'public')
 os.chdir(web_dir)
 
+
+bashCommand = "touch ../hi"
+import subprocess
+process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+output, error = process.communicate()
+
+
 Handler = http.server.SimpleHTTPRequestHandler
 httpd = socketserver.TCPServer(("", PORT), Handler)
 print("serving at port", PORT)
